8cb3ce3b68d3fb12ea0ec60e3c2436b21c93bfe5
luis <luissaavedrafalcon23@gmail.com>
2024-08-26 22:30:35 -0500

    Inicio de proyecto, market consumiendo la api de falabella

---
+++ src/modules/core/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div>LOGO</div>
+        <div>MENU</div>
+        <div>SEARCH</div>
+        <div>LIKE</div>
+        <div>CARD</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
